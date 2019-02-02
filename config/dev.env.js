29c67138acb8d72955ec078870ac7c2655c8722d
david1542 <dudu5321@hotmail.com>
2019-02-02 02:28:08 +0200

    bug fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,5 @@
 module.exports = {
   MONGODB_URI: 'mongodb://localhost:27017/movies-vuejs',
-  // AMQP_URI: 'amqp://localhost',
-  AMQP_URI: 'amqp://ebvxafcv:u5pd8rFngUnQ9j7e_ADDcpub21-iikDu@bee.rmq.cloudamqp.com/ebvxafcv',
-  // d9747ef0-90fb-42d4-aad2-344b5af844f8
+  AMQP_URI: 'amqp://localhost',
   PORT: 4000
 };
